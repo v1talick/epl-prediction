2da3618389f4f98adf33d0f180b3341c14e8f8bf
v1talick <vitalicivanov04@gmail.com>
2023-05-16 12:13:33 +0300

    db fullfil

--- src/main/java/com/OOP/eplpredictions/EplPredictionsApplication.java
+++ src/main/java/com/OOP/eplpredictions/EplPredictionsApplication.java
@@ -1,14 +1,32 @@
 package com.OOP.eplpredictions;
 
+import com.OOP.eplpredictions.entities.Match;
+import com.OOP.eplpredictions.repositories.impl.FootballDataApiRepositoryImpl;
+import com.OOP.eplpredictions.services.MatchService;
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ImportRuntimeHints;
+
+import java.util.List;
 
 @SpringBootApplication
 public class EplPredictionsApplication {
 
+    MatchService matchService;
+
     public static void main(String[] args) {
         SpringApplication.run(EplPredictionsApplication.class, args);
+    }
+    @PostConstruct
+    public void test(){
+        List<Match> matches = new FootballDataApiRepositoryImpl().getAllMatches();
+        matches.forEach(matchService::createMatch);
+    }
+
+    public EplPredictionsApplication(MatchService matchService) {
+        this.matchService = matchService;
     }
 }
